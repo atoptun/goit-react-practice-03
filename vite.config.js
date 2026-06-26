b7f175898980ee0171e56d685b0a468d9e68e6d1
atoptun <topa1978@gmail.com>
2026-06-26 20:24:22 +0300

    Init

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
+import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: { sourcemap: true },
+  resolve: {
+    alias: {
+      '@': path.resolve(import.meta.dirname, './src'),
+    },
+  },
 });
